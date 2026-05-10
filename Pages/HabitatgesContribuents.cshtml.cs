98ef583a107eb7afea4d3894184bf41a39a6a22d
Lluís Lucas <lluislucasfalguera@gmail.com>
2026-05-10 09:30:05 +0200

    asa

--- Pages/HabitatgesContribuents.cshtml.cs
+++ Pages/HabitatgesContribuents.cshtml.cs
@@ -8,6 +8,9 @@ public class HabitatgesContribuentModel : PageModel
     private readonly ILogger<HabitatgesContribuentModel> _logger;
     public Dictionary<string, Dictionary<string, List<JsonNode>>> habitatgesXcp { get; set; } = new();
 
+    public string Cp { get; set; }
+    public string Dni { get; set; }
+
     public HabitatgesContribuentModel(ILogger<HabitatgesContribuentModel> logger)
     {
         _logger = logger;
@@ -15,6 +18,10 @@ public class HabitatgesContribuentModel : PageModel
 
     public async Task OnGetAsync(string dni, string cp)
     {
+        
+
+        Cp = cp;
+        Dni = dni;
         using var http = new HttpClient();
         var json1 = await http.GetStringAsync("http://localhost:5238/llistadhabitatges");
 
@@ -28,20 +35,23 @@ public class HabitatgesContribuentModel : PageModel
             if (cp == cparevisar || cp == null)
             {
 
-            string dniContribuentArevisar = llistaHabitatges[i]["dniContribuent"].ToString();
+                string dniContribuentArevisar = llistaHabitatges[i]["dniContribuent"].ToString();
 
-            if (!habitatgesXcp.ContainsKey(cparevisar))
-            {
-                habitatgesXcp[cparevisar] = new();
+                if (!habitatgesXcp.ContainsKey(cparevisar))
+                {
+                    habitatgesXcp[cparevisar] = new();
+                }
+
+                if (!habitatgesXcp[cparevisar].ContainsKey(dniContribuentArevisar))
+                {
+                    habitatgesXcp[cparevisar][dniContribuentArevisar] = new();
+                }
+                habitatgesXcp[cparevisar][dniContribuentArevisar].Add(llistaHabitatges[i]);
             }
 
-            if (!habitatgesXcp[cparevisar].ContainsKey(dniContribuentArevisar))
-            {
-                habitatgesXcp[cparevisar][dniContribuentArevisar] = new();
-            }
-            habitatgesXcp[cparevisar][dniContribuentArevisar].Add(llistaHabitatges[i]);
-            }
+        }//hem creat el diccionari
 
-}
-}
+
+
+    }
 }
---
+++ Pages/RebutContribuent.cshtml.cs
@@ -0,0 +1,64 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Text.Json.Nodes;
+
+namespace Dinamiques.Pages;
+
+public class RebutContribuentModel : PageModel
+{
+    private readonly ILogger<RebutContribuentModel> _logger;
+    public Dictionary<string, Dictionary<string, List<JsonNode>>> habitatgesXcp { get; set; } = new();
+
+    public string Id {get; set;}
+
+    public JsonNode HabitatgeSeleccionat {get; set;}
+
+    public string Cp { get; set; }
+    public string Dni { get; set; }
+
+    public RebutContribuentModel(ILogger<RebutContribuentModel> logger)
+    {
+        _logger = logger;
+    }
+
+ public async Task OnGetAsync(string dni, string cp)
+    {
+
+        Cp = cp;
+        Dni = dni;
+        using var http = new HttpClient();
+        var json1 = await http.GetStringAsync("http://localhost:5238/llistadhabitatges");
+
+
+        JsonArray llistaHabitatges = JsonNode.Parse(json1)?.AsArray();
+
+        for (int i = 0; i < llistaHabitatges.Count; i++)
+        {
+            string cparevisar = llistaHabitatges[i]["codiPostal"].ToString();
+
+            if (cp == cparevisar || cp == null)
+            {
+
+            string dniContribuentArevisar = llistaHabitatges[i]["dniContribuent"].ToString();
+
+            if (!habitatgesXcp.ContainsKey(cparevisar))
+            {
+                habitatgesXcp[cparevisar] = new();
+            }
+
+            if (!habitatgesXcp[cparevisar].ContainsKey(dniContribuentArevisar))
+            {
+                habitatgesXcp[cparevisar][dniContribuentArevisar] = new();
+            }
+            habitatgesXcp[cparevisar][dniContribuentArevisar].Add(llistaHabitatges[i]);
+            }
+
+        }
+
+         List<JsonNode> habitatgesContribuent = habitatgesXcp[cp][dni];
+
+        
+    }
+}
+        
+    
+
