bb6d91afe5ed2421f9f57d7f8da35071f32917e0
Lluís Lucas <lluislucasfalguera@gmail.com>
2026-05-10 11:04:19 +0200

    afegir edififics i quota

--- Pages/RebutContribuent.cshtml.cs
+++ Pages/RebutContribuent.cshtml.cs
@@ -8,19 +8,29 @@ public class RebutContribuentModel : PageModel
     private readonly ILogger<RebutContribuentModel> _logger;
     public Dictionary<string, Dictionary<string, List<JsonNode>>> habitatgesXcp { get; set; } = new();
 
-    public string Id {get; set;}
+    public string Id { get; set; }
 
-    public JsonNode HabitatgeSeleccionat {get; set;}
+    public JsonNode HabitatgeSeleccionat { get; set; }
 
     public string Cp { get; set; }
     public string Dni { get; set; }
+
+    public int Num_casesTot = 0;
+    public int Num_pisosTot = 0;
+    public int Num_terrenysTot = 0;
+    public bool Descomptetassa = false;
+
+    public int M2casesTot = 0;
+    public int M2pisosTot = 0;
+    public int M2terrenysTot = 0;
+    public double Quotatotal = 0;
 
     public RebutContribuentModel(ILogger<RebutContribuentModel> logger)
     {
         _logger = logger;
     }
 
- public async Task OnGetAsync(string dni, string cp)
+    public async Task OnGetAsync(string dni, string cp)
     {
 
         Cp = cp;
@@ -38,27 +48,57 @@ public class RebutContribuentModel : PageModel
             if (cp == cparevisar || cp == null)
             {
 
-            string dniContribuentArevisar = llistaHabitatges[i]["dniContribuent"].ToString();
+                string dniContribuentArevisar = llistaHabitatges[i]["dniContribuent"].ToString();
 
-            if (!habitatgesXcp.ContainsKey(cparevisar))
-            {
-                habitatgesXcp[cparevisar] = new();
-            }
+                if (!habitatgesXcp.ContainsKey(cparevisar))
+                {
+                    habitatgesXcp[cparevisar] = new();
+                }
 
-            if (!habitatgesXcp[cparevisar].ContainsKey(dniContribuentArevisar))
-            {
-                habitatgesXcp[cparevisar][dniContribuentArevisar] = new();
-            }
-            habitatgesXcp[cparevisar][dniContribuentArevisar].Add(llistaHabitatges[i]);
+                if (!habitatgesXcp[cparevisar].ContainsKey(dniContribuentArevisar))
+                {
+                    habitatgesXcp[cparevisar][dniContribuentArevisar] = new();
+                }
+                habitatgesXcp[cparevisar][dniContribuentArevisar].Add(llistaHabitatges[i]);
             }
 
         }
 
-         List<JsonNode> habitatgesContribuent = habitatgesXcp[cp][dni];
+        List<JsonNode> habitatgesContribuent = habitatgesXcp[cp][dni];
 
-        
+
+        for (int i = 0; i < habitatgesContribuent.Count; i++)
+        {
+            Console.WriteLine(habitatgesContribuent[i]["metresQuadrats"].GetType());
+            if (habitatgesContribuent[i]["tipusImmoble"].ToString() == "0" )// tipusImmoble[0])
+            {
+                int m2DeLaCasa = int.Parse(habitatgesContribuent[i]["metresQuadrats"].ToString());
+                Num_casesTot++;
+                M2casesTot += m2DeLaCasa;
+                int quota_casa= 0.998*m2DeLaCasa;
+                if(habitatgesContribuent[i]["habitantsImmoble"]>=5 )
+                {
+                    
+                }
+            }
+
+            if (habitatgesContribuent[i]["tipusImmoble"].ToString() == "1")
+            {
+                Num_pisosTot++;
+                M2pisosTot += int.Parse(habitatgesContribuent[i]["metresQuadrats"].ToString());
+            }
+
+            if (habitatgesContribuent[i]["tipusImmoble"].ToString() == "2")
+            {
+                Num_terrenysTot++;
+                M2terrenysTot += int.Parse(habitatgesContribuent[i]["metresQuadrats"].ToString());
+            }
+        }
+
+
+
     }
 }
-        
-    
+
+
 
